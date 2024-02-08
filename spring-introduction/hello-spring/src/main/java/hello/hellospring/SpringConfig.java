fc2b6c535277267cf9eafa7fe0e280b8fa72c45a
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-02-08 17:04:16 +0900

    section6 스프링 DB 접근 기술

--- spring-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ spring-introduction/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,19 +1,34 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository()); //단축키 : command + P : 무엇을 넣어줘야 되는지 뜸
+        return new MemberService(memberRepository); //단축키 : command + P : 무엇을 넣어줘야 되는지 뜸
     }
-    @Bean
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository();
+////        return new JdbcMemberRepository(dataSource);
+////        return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
 }
--- spring-introduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ spring-introduction/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -5,10 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService { //단축키 : command + shift + t : 테스트 생성
     private final MemberRepository memberRepository;
 
