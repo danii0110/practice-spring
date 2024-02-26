5e1f9697219a3aa91498f4e16283a1bced12ca5c
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-02-26 14:24:55 +0900

    section8 빈 생명주기 콜백

--- spring-basic/core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-basic/core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,13 +7,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+import static org.springframework.context.annotation.ComponentScan.*;
+
 @Configuration
 @ComponentScan (
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
+
--- spring-basic/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ spring-basic/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
