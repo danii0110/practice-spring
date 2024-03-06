e7461d8e322e23c30b5bbf587357f2a370634dce
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-03-06 10:17:45 +0900

    section2 도메인 분석 설계

---
+++ jpa-fir/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //ORIGINAL 쓰지 말기(나중에 꼬임)
+    private DeliveryStatus status; //READY, COMP
+}
