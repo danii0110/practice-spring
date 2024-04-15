f02298b2c719180b0121ef183a3ac514198892bf
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-04-15 19:06:18 +0900

    section11 객체지향 쿼리 언어2 - 중급 문법

--- jpa-basic/hellojpql/src/main/java/com/jpql/hellojpql/HellojpqlApplication.java
+++ jpa-basic/hellojpql/src/main/java/com/jpql/hellojpql/HellojpqlApplication.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,24 +19,40 @@ public class HellojpqlApplication {
 		tx.begin();
 
 		try {
+			Team teamA = new Team();
+			teamA.setName("팀A");
+			em.persist(teamA);
+
+			Team teamB = new Team();
+			teamB.setName("팀B");
+			em.persist(teamB);
+
 			Member member1 = new Member();
-			member1.setUsername("관리자");
+			member1.setUsername("회원1");
+			member1.setAge(0);
+			member1.setTeam(teamA);
 			em.persist(member1);
 
 			Member member2 = new Member();
-			member2.setUsername("관리자");
+			member2.setUsername("회원2");
+			member2.setAge(0);
+			member2.setTeam(teamA);
 			em.persist(member2);
 
-			em.flush();
+			Member member3 = new Member();
+			member3.setUsername("회원3");
+			member3.setAge(0);
+			member3.setTeam(teamB);
+			em.persist(member3);
+
+			//FLUSH
+			int resultCount = em.createQuery("update Member m set m.age = 20")
+					.executeUpdate();
+			System.out.println("resultCount = " + resultCount);
+
 			em.clear();
-
-			String query = "select locate('de', 'abcdef') From Member m";
-			List<Integer> result = em.createQuery(query, Integer.class)
-					.getResultList();
-
-			for (Integer s : result) {
-				System.out.println("s = " + s);
-			}
+			Member findMember = em.find(Member.class, member1.getId());
+			System.out.println("findMember = " + findMember);
 
 			tx.commit(); //반영해줌
 
--- jpa-basic/hellojpql/src/main/java/com/jpql/hellojpql/Member.java
+++ jpa-basic/hellojpql/src/main/java/com/jpql/hellojpql/Member.java
@@ -3,6 +3,10 @@ package com.jpql.hellojpql;
 import jakarta.persistence.*;
 
 @Entity
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
     @Id @GeneratedValue
     private Long id;
