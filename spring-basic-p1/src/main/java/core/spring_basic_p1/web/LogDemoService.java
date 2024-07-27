f799a4c45c685a574fdb1ba40d2d392b4b06d828
danii0110 <105475360+danii0110@users.noreply.github.com>
2024-07-27 18:46:38 +0900

    section9 빈 스코프

---
+++ spring-basic-p1/src/main/java/core/spring_basic_p1/web/LogDemoService.java
@@ -0,0 +1,14 @@
+package core.spring_basic_p1.web;
+
+import core.spring_basic_p1.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
