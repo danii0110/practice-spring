982d7367be89dbf4a55143f7be30a37e876900a1
danii0110 <danii0110@naver.com>
2025-05-28 17:09:23 +0900

    section7; 스프링 DB 접근 기술 - 22. 스프링 통합 테스트

---
+++ spring-intro/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,51 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberSvc; // 테스트이기 때문에 간단히 하기 위해 @BeforeEach 대신 @Autowired 사용
+    @Autowired MemberRepository memberRepo; // 테스트이기 때문에 간단히 하기 위해 @BeforeEach 대신 @Autowired 사용
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberSvc.join(member);
+
+        // then
+        Member findMember = memberSvc.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberSvc.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberSvc.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
