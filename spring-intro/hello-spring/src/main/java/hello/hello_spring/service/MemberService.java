2a32fc6b9b7eb36fd91c90eb04cae0455eb40ec3
danii0110 <danii0110@naver.com>
2025-05-29 11:27:26 +0900

    section7; 스프링 DB 접근 기술 - 24. JPA

--- spring-intro/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ spring-intro/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,10 +3,12 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 //    private final MemberRepository memberRepo = new MemoryMemberRepository();
     private final MemberRepository memberRepo;
