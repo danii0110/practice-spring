c4e4e83bb58c868f5181c01c03b411d1e90b3a6f
danii0110 <danii0110@naver.com>
2025-05-29 11:52:45 +0900

    section7; 스프링 DB 접근 기술 - 25. 스프링 데이터 JPA

--- spring-intro/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ spring-intro/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -18,23 +18,30 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepo;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepo) {
+        this.memberRepo = memberRepo;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepo);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 }
