891e2783f9fb680d4bdd99dd81dd3580e9cdd96a
danii0110 <danii0110@naver.com>
2025-08-21 17:13:38 +0900

    section9-55

--- spring-basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +26,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { // 의존 관계 주입이 끝나면 호출
+    public void init() { // 의존 관계 주입이 끝나면 호출
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception { // 빈이 종료될 때 호출
+    public void close() { // 빈이 종료될 때 호출
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
